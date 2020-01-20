f121a78b9b6f2a4381b362a64b74e2aa572b0904
Saito <44164993+hatsuki0111@users.noreply.github.com>
2020-01-21 02:33:02 +0900

    Update Tweet.java

--- src/main/java/Tweet.java
+++ src/main/java/Tweet.java
@@ -12,10 +12,10 @@ public class Tweet {
          public static void main(String[] args) {
         ConfigurationBuilder cb = new ConfigurationBuilder();
         cb.setDebugEnabled(true)
-                .setOAuthConsumerKey("dayQHbvKvSd5SpiyD5K6JwKLM")
-                .setOAuthConsumerSecret("zMHk7665aZmimU7SdGVAsOMLyvFJuACoWOVatvuxUj5cdQFxSD")
-                .setOAuthAccessToken("1051757940879765504-p0URTijS6VhCFCf8BO8X3UFQI5z8Ff")
-                .setOAuthAccessTokenSecret("Avu95L2CimMA8Zhg7XJOnoxPbSyyJ1M5hKjYxewMxzjUw");
+                .setOAuthConsumerKey("daokami2938nnmgac0234joppjop32opja30M")
+                .setOAuthConsumerSecret("zMakgapjpnagi389202jklp2309003pappk3209FxSD")
+                .setOAuthAccessToken("105175794aoa2gare33akg04904r904309ralrarneaQI5z8Ff")
+                .setOAuthAccessTokenSecret("Avu95fahfiahfebabaoeo23232hj3njkn9a98989njn3w");
         try {
             TwitterFactory tf = new TwitterFactory(cb.build());
             Twitter twitter = tf.getInstance();
